c7e9fc50f45b87114f10153564f03c958c99b8b7
Sara Avila <lasaraavila1@gmail.com>
2026-03-13 09:25:00 -0600

    Agregar archivos de proyecto.

---
+++ AccesoDatos/Base.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace AccesoDatos
+{
+    public class Base
+    {
+        MySqlConnection con;
+        public Base()
+        {
+            con = new MySqlConnection($"server=localhost; port=3311; user=root; password=12345; database=app_centro_salud;");
+        }
+
+        public void Comando(string query, bool mantenerConexion = false)
+        {
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+
+                if (!mantenerConexion)
+                    con.Close();
+            }
+            catch (Exception)
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+
+                throw;
+            }
+        }
+
+        public DataSet Consultar(string query, string table, bool mantenerConexion = false)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, con);
+                da.Fill(ds, table);
+
+                if (!mantenerConexion)
+                    con.Close();
+            }
+            catch (Exception)
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+
+                throw;
+            }
+
+            return ds;
+        }
+    }
+}
