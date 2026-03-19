293c2f05fae615ca6c93ff73879c5efe9e5b664f
Sara Avila <lasaraavila1@gmail.com>
2026-03-18 19:33:33 -0600

    Entidades Historial clinico y Citas

---
+++ Entidades/HistorialClinico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class HistorialClinico
+    {
+        public HistorialClinico(int idHistorial, int idCita, string diagnostico, string tratamiento, string observaciones, DateTime fechaRegistro)
+        {
+            IdHistorial = idHistorial;
+            IdCita = idCita;
+            Diagnostico = diagnostico;
+            Tratamiento = tratamiento;
+            Observaciones = observaciones;
+            FechaRegistro = fechaRegistro;
+        }
+
+        public int IdHistorial { get; set; }
+        public int IdCita { get; set; }
+        public string Diagnostico { get; set; }
+        public string Tratamiento { get; set; }
+        public string Observaciones { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+    }
+}
