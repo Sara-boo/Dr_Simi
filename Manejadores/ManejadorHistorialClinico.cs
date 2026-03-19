303a05fbfe4a0f911ec0db5a11a4116f4dfb85d7
Sara Avila <lasaraavila1@gmail.com>
2026-03-18 19:44:19 -0600

    Manejador para Historial clinico y Citas

---
+++ Manejadores/ManejadorHistorialClinico.cs
@@ -0,0 +1,25 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace Manejadores
+{
+    public class ManejadorHistorialClinico
+    {
+        Base b = new Base();
+        public void Insertar(HistorialClinico hc)
+        {
+
+            b.Comando($"CALL p_insertar_hc()");
+        }
+
+        public void Editar(HistorialClinico hc)
+        {
+            b.Comando($"CALL p_editar_hc()");
+        }
+    }
+}
