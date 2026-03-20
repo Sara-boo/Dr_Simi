6c6418d7da136443c374a342cd8d4d27dd7c7816
Sara Avila <lasaraavila1@gmail.com>
2026-03-20 10:29:19 -0600

    Entidad y manejador para usuarios

---
+++ Manejadores/ManejadorUsuarios.cs
@@ -0,0 +1,34 @@
+﻿using AccesoDatos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manejadores
+{
+    public class ManejadorUsuarios
+    {
+        Base b = new Base();
+        public void Guardar(Usuarios usuario)
+        {
+            b.Comando($"CALL p_insertar_usuario('{usuario.UserName}', '{usuario.Password}',{usuario.IdPersonal})");
+        }
+
+        public void Editar(Usuarios usuario)
+        {
+            b.Comando($"CALL p_editar_usuario({usuario.IdUsuario}, '{usuario.UserName}', '{usuario.Password}',{usuario.IdPersonal})");
+        }
+
+        public void Estado(Usuarios usuario)
+        {
+            var rs = MessageBox.Show($"Esta seguro de desactivar al usuario {usuario.UserName}", "ATENCION", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (rs == DialogResult.Yes)
+            {
+                b.Comando($"CALL p_estado_usuario({usuario.IdUsuario})");
+            }
+        }
+    }
+}
