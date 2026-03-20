1bd686b8124015f37c0b578acd9f4e48eac5c38b
Marijo M.A <mariajose.ma.0811@gmail.com>
2026-03-20 08:38:34 -0600

    entidades y formulario de registro de medicamento

---
+++ Entidades/Medicamentos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Medicamentos
+    {
+        public Medicamentos(int idMedicamento, string nombre, string descripcion, string tipo, string presentacion, string concentracion, bool requiereReceta)
+        {
+            IdMedicamento = idMedicamento;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Tipo = tipo;
+            Presentacion = presentacion;
+            Concentracion = concentracion;
+            RequiereReceta = requiereReceta;
+        }
+
+        public int IdMedicamento { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Tipo { get; set; }
+        public string Presentacion { get; set; }
+        public string Concentracion { get; set; }
+        public bool RequiereReceta { get; set; }
+    }
+}
---
+++ Entidades/MovimientosInventario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class MovimientosInventario
+    {
+        public MovimientosInventario(int idMovimiento, int fkidMovimiento, string tipoMovimiento, int cantidad, string motivo, DateTime fecha, int fkidUsuario)
+        {
+            IdMovimiento = idMovimiento;
+            FkidMovimiento = fkidMovimiento;
+            TipoMovimiento = tipoMovimiento;
+            Cantidad = cantidad;
+            Motivo = motivo;
+            Fecha = fecha;
+            FkidUsuario = fkidUsuario;
+        }
+
+        public int IdMovimiento { get; set; }
+        public int FkidMovimiento { get; set; }
+        public string TipoMovimiento { get; set; }
+        public int Cantidad { get; set; }
+        public string Motivo { get; set; }
+        public DateTime Fecha { get; set; }
+        public int FkidUsuario { get; set; }
+    }
+}
