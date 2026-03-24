3eeb3c882480f4d233a3deb564d2af7aad0d4782
ferISC5 <lm230110546@lagos.tecmm.edu.mx>
2026-03-24 11:51:38 -0600

    Clase y forms personal, sin funcionalidad

---
+++ LogClinic/FrmPersonal.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace LogClinic
+{
+    partial class FrmPersonal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.DtgDatosPersonal = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DtgDatosPersonal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del empleado";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(296, 91);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(987, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(93)))), ((int)(((byte)(126)))));
+            this.BtnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnBuscar.ForeColor = System.Drawing.Color.Transparent;
+            this.BtnBuscar.Location = new System.Drawing.Point(1305, 84);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(106, 44);
+            this.BtnBuscar.TabIndex = 2;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(93)))), ((int)(((byte)(126)))));
+            this.BtnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAgregar.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgregar.ForeColor = System.Drawing.Color.Transparent;
+            this.BtnAgregar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnAgregar.Location = new System.Drawing.Point(1427, 82);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(60, 46);
+            this.BtnAgregar.TabIndex = 3;
+            this.BtnAgregar.Text = "+";
+            this.BtnAgregar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // DtgDatosPersonal
+            // 
+            this.DtgDatosPersonal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DtgDatosPersonal.Location = new System.Drawing.Point(56, 168);
+            this.DtgDatosPersonal.Name = "DtgDatosPersonal";
+            this.DtgDatosPersonal.RowHeadersWidth = 51;
+            this.DtgDatosPersonal.RowTemplate.Height = 24;
+            this.DtgDatosPersonal.Size = new System.Drawing.Size(1431, 594);
+            this.DtgDatosPersonal.TabIndex = 4;
+            // 
+            // FrmPersonal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 45F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(236)))), ((int)(((byte)(238)))));
+            this.ClientSize = new System.Drawing.Size(1537, 835);
+            this.Controls.Add(this.DtgDatosPersonal);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.BtnBuscar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 8, 4, 8);
+            this.Name = "FrmPersonal";
+            this.Text = "FrmPersonal";
+            ((System.ComponentModel.ISupportInitialize)(this.DtgDatosPersonal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.DataGridView DtgDatosPersonal;
+    }
+}
