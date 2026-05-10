ca32b5cb6e9dadba52a9543c6748dc2d3b6b1cf9
Sara Avila <lasaraavila1@gmail.com>
2026-05-09 19:56:44 -0600

    Rediseño del Formlario Usuarios listo

--- LogClinic/FrmVerUsuarios.Designer.cs
+++ LogClinic/FrmVerUsuarios.Designer.cs
@@ -42,12 +42,13 @@
             this.DtgDatos.AllowUserToAddRows = false;
             this.DtgDatos.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.DtgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DtgDatos.Location = new System.Drawing.Point(59, 220);
+            this.DtgDatos.Location = new System.Drawing.Point(44, 179);
+            this.DtgDatos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DtgDatos.Name = "DtgDatos";
             this.DtgDatos.ReadOnly = true;
             this.DtgDatos.RowHeadersWidth = 51;
             this.DtgDatos.RowTemplate.Height = 24;
-            this.DtgDatos.Size = new System.Drawing.Size(880, 377);
+            this.DtgDatos.Size = new System.Drawing.Size(660, 306);
             this.DtgDatos.TabIndex = 14;
             this.DtgDatos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatos_CellClick);
             this.DtgDatos.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgDatos_CellEnter);
@@ -58,9 +59,10 @@
             this.BtnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnAgregar.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnAgregar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
-            this.BtnAgregar.Location = new System.Drawing.Point(897, 136);
+            this.BtnAgregar.Location = new System.Drawing.Point(673, 110);
+            this.BtnAgregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnAgregar.Name = "BtnAgregar";
-            this.BtnAgregar.Size = new System.Drawing.Size(47, 52);
+            this.BtnAgregar.Size = new System.Drawing.Size(35, 42);
             this.BtnAgregar.TabIndex = 13;
             this.BtnAgregar.Text = "+";
             this.BtnAgregar.UseVisualStyleBackColor = false;
@@ -72,9 +74,10 @@
             this.BtnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnBuscar.Font = new System.Drawing.Font("Javanese Text", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnBuscar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
-            this.BtnBuscar.Location = new System.Drawing.Point(743, 136);
+            this.BtnBuscar.Location = new System.Drawing.Point(557, 110);
+            this.BtnBuscar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BtnBuscar.Name = "BtnBuscar";
-            this.BtnBuscar.Size = new System.Drawing.Size(131, 52);
+            this.BtnBuscar.Size = new System.Drawing.Size(98, 42);
             this.BtnBuscar.TabIndex = 12;
             this.BtnBuscar.Text = "Buscar";
             this.BtnBuscar.UseVisualStyleBackColor = false;
@@ -84,19 +87,21 @@
             // 
             this.TxtBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(249)))), ((int)(((byte)(255)))));
             this.TxtBuscar.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtBuscar.Location = new System.Drawing.Point(302, 136);
+            this.TxtBuscar.Location = new System.Drawing.Point(226, 110);
+            this.TxtBuscar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.TxtBuscar.Multiline = true;
             this.TxtBuscar.Name = "TxtBuscar";
-            this.TxtBuscar.Size = new System.Drawing.Size(415, 52);
+            this.TxtBuscar.Size = new System.Drawing.Size(312, 43);
             this.TxtBuscar.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Javanese Text", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(48, 136);
+            this.label1.Location = new System.Drawing.Point(36, 110);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(256, 57);
+            this.label1.Size = new System.Drawing.Size(206, 45);
             this.label1.TabIndex = 10;
             this.label1.Text = "Nombre del Usuario: ";
             // 
@@ -104,18 +109,19 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Lucida Bright", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(25, 23);
+            this.label2.Location = new System.Drawing.Point(19, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(161, 39);
+            this.label2.Size = new System.Drawing.Size(129, 32);
             this.label2.TabIndex = 15;
             this.label2.Text = "Usuarios";
             // 
             // FrmVerUsuarios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(236)))), ((int)(((byte)(238)))));
-            this.ClientSize = new System.Drawing.Size(1014, 645);
+            this.ClientSize = new System.Drawing.Size(760, 524);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.DtgDatos);
             this.Controls.Add(this.BtnAgregar);
@@ -123,7 +129,9 @@
             this.Controls.Add(this.TxtBuscar);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmVerUsuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmVerUsuarios";
             this.Load += new System.EventHandler(this.FrmVerUsuarios_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DtgDatos)).EndInit();
