3b86b2b7ed1126c668a5c7cf8f01dd628ce6e9d6
rosad <rosad@COLLAZO>
2026-05-10 11:22:50 -0600

    Mejoras en UI de formularios de pacientes
    Se ajustaron tamaños y posiciones de controles en FrmPacientes y FrmRegistroPacientes para optimizar la distribución visual y el área de edición. Se aumentó el tamaño de los formularios y se reubicaron botones y campos. Además, se cambió StartPosition a CenterParent en FrmRegistroPacientes y se añadió un manejador vacío para el evento Click de label12.

--- LogClinic/FrmPacientes.Designer.cs
+++ LogClinic/FrmPacientes.Designer.cs
@@ -47,7 +47,7 @@
             this.DtgPaciente.ReadOnly = true;
             this.DtgPaciente.RowHeadersWidth = 51;
             this.DtgPaciente.RowTemplate.Height = 24;
-            this.DtgPaciente.Size = new System.Drawing.Size(1078, 465);
+            this.DtgPaciente.Size = new System.Drawing.Size(1283, 604);
             this.DtgPaciente.TabIndex = 0;
             this.DtgPaciente.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgPaciente_CellClick);
             this.DtgPaciente.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DtgPaciente_CellContentClick);
@@ -56,7 +56,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Bright", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 37);
+            this.label1.Location = new System.Drawing.Point(50, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(241, 29);
             this.label1.TabIndex = 4;
@@ -65,9 +65,9 @@
             // TxtBCurp
             // 
             this.TxtBCurp.Font = new System.Drawing.Font("Javanese Text", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtBCurp.Location = new System.Drawing.Point(279, 33);
+            this.TxtBCurp.Location = new System.Drawing.Point(297, 40);
             this.TxtBCurp.Name = "TxtBCurp";
-            this.TxtBCurp.Size = new System.Drawing.Size(627, 30);
+            this.TxtBCurp.Size = new System.Drawing.Size(782, 30);
             this.TxtBCurp.TabIndex = 5;
             this.TxtBCurp.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtBCurp_KeyPress);
             // 
@@ -77,7 +77,7 @@
             this.BtnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnBuscar.Font = new System.Drawing.Font("Javanese Text", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnBuscar.ForeColor = System.Drawing.Color.White;
-            this.BtnBuscar.Location = new System.Drawing.Point(914, 28);
+            this.BtnBuscar.Location = new System.Drawing.Point(1101, 35);
             this.BtnBuscar.Name = "BtnBuscar";
             this.BtnBuscar.Size = new System.Drawing.Size(96, 38);
             this.BtnBuscar.TabIndex = 6;
@@ -96,7 +96,7 @@
             this.BtnReporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnReporte.Font = new System.Drawing.Font("Javanese Text", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnReporte.ForeColor = System.Drawing.Color.White;
-            this.BtnReporte.Location = new System.Drawing.Point(1055, 19);
+            this.BtnReporte.Location = new System.Drawing.Point(1258, 26);
             this.BtnReporte.Margin = new System.Windows.Forms.Padding(0);
             this.BtnReporte.Name = "BtnReporte";
             this.BtnReporte.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -114,7 +114,7 @@
             this.BtnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnAgregar.Font = new System.Drawing.Font("Javanese Text", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnAgregar.ForeColor = System.Drawing.Color.White;
-            this.BtnAgregar.Location = new System.Drawing.Point(1016, 28);
+            this.BtnAgregar.Location = new System.Drawing.Point(1219, 35);
             this.BtnAgregar.Name = "BtnAgregar";
             this.BtnAgregar.Size = new System.Drawing.Size(36, 38);
             this.BtnAgregar.TabIndex = 7;
@@ -127,7 +127,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(236)))), ((int)(((byte)(238)))));
-            this.ClientSize = new System.Drawing.Size(1165, 695);
+            this.ClientSize = new System.Drawing.Size(1363, 774);
             this.Controls.Add(this.BtnReporte);
             this.Controls.Add(this.BtnAgregar);
             this.Controls.Add(this.BtnBuscar);
